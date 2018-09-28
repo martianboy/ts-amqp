fab91e6165982eb92753a3bb096bb0deccdce518
Abbas Mashayekh <martianboy2005@gmail.com>
2018-09-28 15:44:06 +0330

    Extracted rpc into a ChannelRPC class to re-use in other classes

--- classes/Exchange.ts
+++ classes/Exchange.ts
@@ -1,63 +1,29 @@
 import { EventEmitter } from "events";
+
 import { IExchange } from "../interfaces/Exchange";
-import { IChannel } from "../interfaces/Channel";
 import { ICloseReason, EAMQPClasses } from "../interfaces/Protocol";
+import ChannelRPC from "../utils/ChannelRPC";
+import { IChannel } from "../interfaces/Channel";
 
-export const EXCHANGE_CLASS = 40;
+const EXCHANGE_DECLARE = 10;
+const EXCHANGE_DECLARE_OK = 11;
 
-export const EXCHANGE_DECLARE = 10;
-export const EXCHANGE_DECLARE_OK = 11;
-
-export const EXCHANGE_DELETE = 20;
-export const EXCHANGE_DELETE_OK = 21;
+const EXCHANGE_DELETE = 20;
+const EXCHANGE_DELETE_OK = 21;
 
 export class ExchangeNotFoundError extends Error {}
 export class ExchangeInUseError extends Error {}
 
 export class Exchange extends EventEmitter {
-    public constructor(private ch: IChannel) {
+    rpc: ChannelRPC;
+
+    constructor(ch: IChannel) {
         super();
-    }
-
-    private expectCommand(method: number, callback: (...args: any[]) => void) {
-        this.ch.once(`method:${EXCHANGE_CLASS}:${method}`, callback);
-    }
-
-    private rpc<T>(method: number, resp_method: number, args: any): Promise<T> {
-        var already_resolved = false;
-
-        return new Promise((resolve, reject) => {
-            this.ch.sendMethod(EXCHANGE_CLASS, method, args);
-            if (args.no_wait) {
-                already_resolved = true;
-                return resolve();
-            }
-            else {
-                this.expectCommand(resp_method, (args: any) => {
-                    already_resolved = true;
-                    this.ch.off('closing', onError);
-                    resolve(args);
-                });
-            }
-
-            const onError = (reason: ICloseReason) => {
-                if (already_resolved) return;
-
-                if (
-                    reason.class_id === EAMQPClasses.EXCHANGE &&
-                    reason.method_id === method &&
-                    reason.reply_code >= 400            // Is it possible that server closes with a < 400 code?
-                ) {
-                    reject(reason);
-                }
-            }
-
-            this.ch.once('closing', onError);
-        });
+        this.rpc = new ChannelRPC(ch, EAMQPClasses.EXCHANGE);
     }
 
     public declare(exchange: IExchange, passive: boolean = false, no_wait: boolean = false): Promise<void> {
-        return this.rpc<void>(EXCHANGE_DECLARE, EXCHANGE_DECLARE_OK, {
+        return this.rpc.call<void>(EXCHANGE_DECLARE, EXCHANGE_DECLARE_OK, {
             reserved1: 0,
             exchange: exchange.name,
             type: exchange.type,
@@ -73,7 +39,7 @@ export class Exchange extends EventEmitter {
     }
 
     public delete(name: string, if_unused: boolean = true, no_wait: boolean = false): Promise<void> {
-        return this.rpc<void>(EXCHANGE_DELETE, EXCHANGE_DELETE_OK, {
+        return this.rpc.call<void>(EXCHANGE_DELETE, EXCHANGE_DELETE_OK, {
             reserved1: 0,
             exchange: name,
             if_unused: if_unused,
---
+++ utils/ChannelRPC.ts
@@ -0,0 +1,43 @@
+import { ICloseReason, EAMQPClasses } from "../interfaces/Protocol";
+import { IChannel } from "../interfaces/Channel";
+
+export default class ChannelRPC {
+    public constructor(protected ch: IChannel, protected class_id: EAMQPClasses) {}
+
+    public expectCommand(method: number, callback: (...args: any[]) => void) {
+        this.ch.once(`method:${this.class_id}:${method}`, callback);
+    }
+
+    public call<T>(method: number, resp_method: number, args: any): Promise<T> {
+        var already_resolved = false;
+
+        return new Promise((resolve, reject) => {
+            this.ch.sendMethod(this.class_id, method, args);
+            if (args.no_wait) {
+                already_resolved = true;
+                return resolve();
+            }
+            else {
+                this.expectCommand(resp_method, (args: any) => {
+                    already_resolved = true;
+                    this.ch.off('closing', onError);
+                    resolve(args);
+                });
+            }
+
+            const onError = (reason: ICloseReason) => {
+                if (already_resolved) return;
+
+                if (
+                    reason.class_id === EAMQPClasses.EXCHANGE &&
+                    reason.method_id === method &&
+                    reason.reply_code >= 400            // Is it possible that server closes with a < 400 code?
+                ) {
+                    reject(reason);
+                }
+            }
+
+            this.ch.once('closing', onError);
+        });
+    }
+}
