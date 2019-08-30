dedf13ba842d3dddaaee699d936aa5660862c07f
Abbas Mashayekh <martianboy2005@gmail.com>
2019-08-30 18:34:25 +0430

    fix: work around a nasty concurrency issue with publish

--- src/classes/JsonPublisher.ts
+++ src/classes/JsonPublisher.ts
@@ -11,21 +11,23 @@ export class JsonPublisher extends Transform {
     }
 
     _transform(message: IMessage<unknown>, _encoding: string, cb: TransformCallback) {
-        cb(undefined, {
-            class_id: EAMQPClasses.BASIC,
-            method_id: BASIC_PUBLISH,
-            args: {
-                reserved1: 0,
-                exchange_name: message.exchange || '',
-                routing_key: message.routing_key,
-                mandatory: message.mandatory || false,
-                immediate: message.immediate || false
-            },
-            properties: {
-                ...message.properties,
-                contentType: 'application/json'
-            },
-            body: Buffer.from(JSON.stringify(message.body))
+        setImmediate(() => {
+            cb(undefined, {
+                class_id: EAMQPClasses.BASIC,
+                method_id: BASIC_PUBLISH,
+                args: {
+                    reserved1: 0,
+                    exchange_name: message.exchange || '',
+                    routing_key: message.routing_key,
+                    mandatory: message.mandatory || false,
+                    immediate: message.immediate || false
+                },
+                properties: {
+                    ...message.properties,
+                    contentType: 'application/json'
+                },
+                body: Buffer.from(JSON.stringify(message.body))
+            });
         });
     }
 }
